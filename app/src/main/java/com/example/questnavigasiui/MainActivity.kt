c501b1fb29471da2e92e8bef6b81ce4627392b8c
mauraaninditakirana <kiranamaura351@gmail.com>
2025-11-06 14:22:36 +0700

    Menghapus Greeting dan memanggil DataApp di dalam Scaffold.

--- app/src/main/java/com/example/questnavigasiui/MainActivity.kt
+++ app/src/main/java/com/example/questnavigasiui/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.questnavigasiui
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -14,16 +15,15 @@ import androidx.compose.ui.tooling.preview.Preview
 import com.example.questnavigasiui.ui.theme.QuestNavigasiUITheme
 
 class MainActivity : ComponentActivity() {
+    @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             QuestNavigasiUITheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                Scaffold(modifier = Modifier.fillMaxSize()) {
+                    //Kode program bagian yang diedit
+                    DataApp(modifier = Modifier)
                 }
             }
         }
