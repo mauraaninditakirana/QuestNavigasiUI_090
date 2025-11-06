e58ec24d29dbaefc1bc34b97f2f011413a32d478
mauraaninditakirana <kiranamaura351@gmail.com>
2025-11-06 13:27:40 +0700

    Menambahkan Column di dalam Scaffold dengan padding, verticalArrangement, dan horizontalAlignment

--- app/src/main/java/com/example/questnavigasiui/view/Formulir.kt
+++ app/src/main/java/com/example/questnavigasiui/view/Formulir.kt
@@ -2,12 +2,16 @@
 
 package com.example.questnavigasiui.view
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.colorResource
@@ -27,6 +31,14 @@ fun FormIsian(
                 colors = TopAppBarDefaults.topAppBarColors
                     (colorResource(id = R.color.teal_700))
             )}
-        ){}
+        ){ isiRuang ->
+            Column(modifier = Modifier.padding(isiRuang),
+                verticalArrangement = Arrangement.Top,
+                horizontalAlignment = Alignment.CenterHorizontally)
+            {
+
+            }
+
+    }
 }
 
