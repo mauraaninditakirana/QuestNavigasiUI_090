205840b709d804c987273939f01b3ad59c862e9f
mauraaninditakirana <kiranamaura351@gmail.com>
2025-11-06 13:12:34 +0700

    Menambahkan dependensi Material3 dan membuat Scaffold dengan TopAppBar di file Formulir.kt.

--- app/src/main/java/com/example/questnavigasiui/view/Formulir.kt
+++ app/src/main/java/com/example/questnavigasiui/view/Formulir.kt
@@ -2,15 +2,20 @@
 
 package com.example.questnavigasiui.view
 
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 
 @Composable
 fun FormIsian(
     jenisK:List<String> = listOf("Laki-laki", "Perempuan"),
     OnSUbmitBtnClick: () -> Unit
 ){
-
+    Scaffold (modifier= Modifier,
+        {
+            TopAppBar()
+        })
 }
 
