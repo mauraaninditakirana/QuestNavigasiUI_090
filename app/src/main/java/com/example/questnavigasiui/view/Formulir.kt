e70517e990c67df6b07560e1aa90b0c7a6e8c607
mauraaninditakirana <kiranamaura351@gmail.com>
2025-11-06 13:34:36 +0700

    Menambahkan HorizontalDivider dan Row di dalam file Formulir.kt

--- app/src/main/java/com/example/questnavigasiui/view/Formulir.kt
+++ app/src/main/java/com/example/questnavigasiui/view/Formulir.kt
@@ -4,9 +4,12 @@ package com.example.questnavigasiui.view
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.material3.DividerDefaults.Thickness
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
@@ -51,6 +54,12 @@ fun FormIsian(
 
 
                 )
+                HorizontalDivider(modifier = Modifier
+                    .padding(20.dp)
+                    .width(250.dp), thickness = Thickness, color = Color.Red)
+                Row{
+                    jenisK.forEach{}
+                }
             }
 
     }
