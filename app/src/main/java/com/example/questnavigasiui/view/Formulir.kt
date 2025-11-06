2855a5e1997bcb0d5ff0ae789b4b397eb42fe71c
mauraaninditakirana <kiranamaura351@gmail.com>
2025-11-06 13:46:17 +0700

    Menambahkan teks "Submit" pada tombol di halaman Formulir.

--- app/src/main/java/com/example/questnavigasiui/view/Formulir.kt
+++ app/src/main/java/com/example/questnavigasiui/view/Formulir.kt
@@ -93,7 +93,7 @@ fun FormIsian(
                     modifier = Modifier.fillMaxWidth(1f),
                     onClick = OnSUbmitBtnClick
                 ){
-
+                    Text(stringResource(id = R.string.submit))
                 }
             }
 
